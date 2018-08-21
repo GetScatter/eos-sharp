79c25ea319cf15f911a8f8c289739c7ad06aa285
Mário Silva <mmcs@outlook.pt>
2018-08-21 22:09:17 +0100

    add missing type writers

--- EosSharp/EosSharp/Providers/AbiSerializationProvider.cs
+++ EosSharp/EosSharp/Providers/AbiSerializationProvider.cs
@@ -20,22 +20,41 @@ namespace EosSharp.Providers
             this.Api = api;
 
             TypeWriters = new Dictionary<string, Action<MemoryStream, object>>()
-            {
-                { "string", WriteString },
-                { "uint8",  WriteUint8 },
-                { "uint16", WriteUint16 },
-                { "uint32", WriteUint32 },
-                { "uint64", WriteUint8 },
-                { "varuint32", WriteVarUint32 },
-                { "varint32", WriteVarInt32 },
-                { "float32", WriteFloat32 },
-                { "float64", WriteFloat64 },
-                { "account_name", WriteName },
-                { "asset", WriteAsset },
-                { "bytes", WriteBytes },
-                //TODO add missing types and array support
+            {     
+                {"int8",                 WriteInt8               },
+                {"uint8",                WriteUint8              },
+                {"int16",                WriteInt16              },
+                {"uint16",               WriteUint16             },
+                {"int32",                WriteInt32              },
+                {"uint32",               WriteUint32             },
+                {"int64",                WriteInt64              },
+                {"uint64",               WriteUint64             },
+                {"int128",               WriteInt128             },
+                {"uint128",              WriteUInt128            },
+                {"varuint32",            WriteVarUint32          },
+                {"varint32",             WriteVarInt32           },
+                {"float32",              WriteFloat32            },
+                {"float64",              WriteFloat64            },
+                {"float128",             WriteFloat128           },
+                {"bytes",                WriteBytes              },
+                {"bool",                 WriteBool               },
+                {"string",               WriteString             },
+                {"name",                 WriteName               },
+                {"asset",                WriteAsset              },
+                {"time_point",           WriteTimePoint          },
+                {"time_point_sec",       WriteTimePointSec       },
+                {"block_timestamp_type", WriteBlockTimestampType },
+                {"symbol_code",          WriteSymbolCode         },
+                {"symbol",               WriteSymbol             },
+                {"checksum160",          WriteChecksum160        },
+                {"checksum256",          WriteChecksum256        },
+                {"checksum512",          WriteChecksum512        },
+                {"public_key",           WritePublicKey          },
+                {"private_key",          WritePrivateKey         },
+                {"signature",            WriteSignature          },
+                {"extended_asset",       WriteExtendedAsset      }
             };
-        }
+        }      
 
         public async Task<byte[]> SerializePackedTransaction(Transaction trx)
         {
@@ -116,36 +135,95 @@ namespace EosSharp.Providers
             return Task.WhenAll(abiTasks);
         }
 
-        private void WriteExtension(MemoryStream ms, Api.v1.Extension extension)
+        #region Writer Functions
+
+        private static void WriteInt8(MemoryStream ms, object value)
         {
-            WriteUint16(ms, extension.Type);
-            //TODO abi data?
-            //public object Data { get; set; }
+            throw new NotImplementedException();
         }
 
-        private void WriteAction(MemoryStream ms, Api.v1.Action action, Abi abi)
+        private static void WriteUint8(MemoryStream ms, object value)
         {
-            WriteName(ms, action.Account);
-            WriteName(ms, action.Name);
+            ms.Write(new byte[] { (byte)value }, 0, 1);
+        }
 
-            WriteVarUint32(ms, (UInt32)action.Authorization.Count);
-            foreach (var perm in action.Authorization)
+        private static void WriteInt16(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteUint16(MemoryStream ms, object value)
+        {
+            ms.Write(BitConverter.GetBytes((UInt16)value), 0, 2);
+        }
+
+        private static void WriteInt32(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteUint32(MemoryStream ms, object value)
+        {
+            ms.Write(BitConverter.GetBytes((UInt32)value), 0, 4);
+        }
+
+        private static void WriteInt64(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteUint64(MemoryStream ms, object value)
+        {
+            ms.Write(BitConverter.GetBytes((UInt64)value), 0, 8);
+        }
+
+        private static void WriteUInt128(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteInt128(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteVarUint32(MemoryStream ms, object value)
+        {
+            var v = (UInt32)value;
+            while (true)
             {
-                WritePermissionLevel(ms, perm);
+                if ((v >> 7) != 0)
+                {
+                    ms.Write(new byte[] { (byte)(0x80 | (v & 0x7f)) }, 0, 1);
+                    v >>= 7;
+                }
+                else
+                {
+                    ms.Write(new byte[] { (byte)(v) }, 0, 1);
+                    break;
+                }
             }
-
-            WriteBytes(ms, SerializeActionData(action, abi));
         }
 
-        private void WriteAbiType(MemoryStream ms, object value, string type, Abi abi)
+        private static void WriteVarInt32(MemoryStream ms, object value)
         {
-            TypeWriters[type](ms, value);
+            var n = (Int32)value;
+            WriteVarUint32(ms, (UInt32)((n << 1) ^ (n >> 31)));
         }
 
-        private static void WritePermissionLevel(MemoryStream ms, PermissionLevel perm)
+        private static void WriteFloat32(MemoryStream ms, object value)
         {
-            WriteName(ms, perm.Actor);
-            WriteName(ms, perm.Permission);
+            ms.Write(BitConverter.GetBytes((float)value), 0, 4);
+        }
+
+        private static void WriteFloat64(MemoryStream ms, object value)
+        {
+            ms.Write(BitConverter.GetBytes((double)value), 0, 8);
+        }
+
+        private static void WriteFloat128(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
         }
 
         private static void WriteBytes(MemoryStream ms, object value)
@@ -154,6 +232,19 @@ namespace EosSharp.Providers
 
             WriteVarUint32(ms, (UInt32)bytes.Length);
             ms.Write(bytes, 0, bytes.Length);
+        }
+
+        private static void WriteBool(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteString(MemoryStream ms, object value)
+        {
+            string s = (string)value;
+            WriteVarUint32(ms, (UInt32)s.Length);
+            if (s.Length > 0)
+                ms.Write(Encoding.UTF8.GetBytes(s), 0, s.Length);
         }
 
         private static void WriteName(MemoryStream ms, object value)
@@ -220,6 +311,21 @@ namespace EosSharp.Providers
             WriteSymbol(ms, new Symbol() { Name = name, Precision = precision });
         }
 
+        private static void WriteTimePoint(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteTimePointSec(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteBlockTimestampType(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
         private static void WriteSymbol(MemoryStream ms, object value)
         {
             var symbol = (Symbol)value;
@@ -242,66 +348,112 @@ namespace EosSharp.Providers
             }
         }
 
-        private static void WriteString(MemoryStream ms, object value)
+        private static void WriteSymbolCode(MemoryStream ms, object value)
         {
-            string s = (string)value;
-            WriteVarUint32(ms, (UInt32)s.Length);
-            if(s.Length > 0)
-                ms.Write(Encoding.UTF8.GetBytes(s), 0, s.Length);
+            throw new NotImplementedException();
         }
 
-        public static void WriteUint8(MemoryStream ms, object value)
+        private static void WriteChecksum160(MemoryStream ms, object value)
         {
-            ms.Write(new byte[] { (byte)value }, 0, 1);
+            throw new NotImplementedException();
         }
 
-        private static void WriteUint16(MemoryStream ms, object value)
+        private static void WriteChecksum256(MemoryStream ms, object value)
         {
-            ms.Write(BitConverter.GetBytes((UInt16)value), 0, 2);
+            throw new NotImplementedException();
         }
 
-        private static void WriteUint32(MemoryStream ms, object value)
+        private static void WriteChecksum512(MemoryStream ms, object value)
         {
-            ms.Write(BitConverter.GetBytes((UInt32)value), 0, 4);
+            throw new NotImplementedException();
+        }
+        
+        private static void WritePublicKey(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
         }
 
-        private static void WriteVarUint32(MemoryStream ms, object value)
+        private static void WritePrivateKey(MemoryStream ms, object value)
         {
-            var v = (UInt32)value;
-            while (true)
+            throw new NotImplementedException();
+        }
+
+        private static void WriteSignature(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteExtendedAsset(MemoryStream ms, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WriteExtension(MemoryStream ms, Api.v1.Extension extension)
+        {
+            WriteUint16(ms, extension.Type);
+            //TODO abi data?
+            //public object Data { get; set; }
+        }
+
+        private void WriteAction(MemoryStream ms, Api.v1.Action action, Abi abi)
+        {
+            WriteName(ms, action.Account);
+            WriteName(ms, action.Name);
+
+            WriteVarUint32(ms, (UInt32)action.Authorization.Count);
+            foreach (var perm in action.Authorization)
             {
-                if ((v >> 7) != 0)
-                {
-                    ms.Write(new byte[] { (byte)(0x80 | (v & 0x7f)) }, 0, 1);
-                    v >>= 7;
-                }
-                else
-                {
-                    ms.Write(new byte[] { (byte)(v) }, 0, 1);
-                    break;
-                }
+                WritePermissionLevel(ms, perm);
             }
+
+            WriteBytes(ms, SerializeActionData(action, abi));
         }
 
-        private static void WriteVarInt32(MemoryStream ms, object value)
+        private void WriteAbiType(MemoryStream ms, object value, string type, Abi abi)
         {
-            var n = (Int32)value;
-            WriteVarUint32(ms, (UInt32)((n << 1) ^ (n >> 31)));
+
+
+            TypeWriters[type](ms, value);
         }
 
-        private static void WriteFloat32(MemoryStream ms, object value)
+        //    function serializeStruct(buffer: SerialBuffer, data: any)
+        //    {
+        //        if (this.base)
+        //this.base.serialize(buffer, data);
+        //        for (let field of this.fields)
+        //        {
+        //            if (!(field.name in data))
+        //  throw new Error('missing ' + this.name + '.' + field.name + ' (type=' + field.type.name + ')');
+        //        field.type.serialize(buffer, data[field.name]);
+        //    }
+        //}
+
+        //function serializeArray(buffer: SerialBuffer, data: any[])
+        //{
+        //    buffer.pushVaruint32(data.length);
+        //    for (let item of data)
+        //        this.arrayOf.serialize(buffer, item);
+        //}
+
+        //function serializeOptional(buffer: SerialBuffer, data: any)
+        //{
+        //    if (data === null || data === undefined)
+        //    {
+        //        buffer.push(0);
+        //    }
+        //    else
+        //    {
+        //        buffer.push(1);
+        //        this.optionalOf.serialize(buffer, data);
+        //    }
+        //}
+
+        private static void WritePermissionLevel(MemoryStream ms, PermissionLevel perm)
         {
-            ms.Write(BitConverter.GetBytes((float)value), 0, 4);
+            WriteName(ms, perm.Actor);
+            WriteName(ms, perm.Permission);
         }
 
-        private static void WriteFloat64(MemoryStream ms, object value)
-        {
-            ms.Write(BitConverter.GetBytes((double)value), 0, 8);
-        }
-
-        private static void WriteUint64(MemoryStream ms, object value)
-        {
-            ms.Write(BitConverter.GetBytes((UInt64)value), 0, 8);
-        }
+        #endregion
     }
 }
