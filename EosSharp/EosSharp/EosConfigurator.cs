c3661992144d1c9ff57b08137c3ca46e05ed4006
Mário Silva <mario.silva@ocidental.pt>
2018-08-24 10:32:12 +0100

    remove unused flags

--- EosSharp/EosSharp/EosConfigurator.cs
+++ EosSharp/EosSharp/EosConfigurator.cs
@@ -9,8 +9,6 @@ namespace EosSharp
         public string HttpEndpoint { get; set; } = "http://127.0.0.1:8888";
         public string ChainId { get; set; }
         public double ExpireSeconds { get; set; } = 60;
-        public bool Broadcast { get; set; } = true;
-        public bool Sign { get; set; } = true;
         public ISignProvider SignProvider { get; set; }
     }
 }
