be8286c120c463b50c6ad204f0bec5df8b47adda
Mário Silva <mario.silva@ocidental.pt>
2018-09-04 15:16:29 +0100

    adding serialization unittests

---
+++ EosSharp/EosSharp.UnitTests/SerializationUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using EosSharp.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EosSharp.UnitTests
+{
+    [TestClass]
+    public class SerializationUnitTests
+    {
+        [TestMethod]
+        [TestCategory("Serialization Tests")]
+        public void DoubleSerialization()
+        {
+            string doubleStr = "10001";
+            var doubleBytes = SerializationHelper.DecimalToBinary(8, doubleStr);
+            var doubleBytes2 = BitConverter.GetBytes(Convert.ToUInt64(doubleStr));
+            var doubleResStr = SerializationHelper.BinaryToDecimal(doubleBytes);
+            Assert.IsTrue(doubleStr == doubleResStr && doubleBytes.SequenceEqual(doubleBytes2));
+        }
+
+        [TestMethod]
+        [TestCategory("Serialization Tests")]
+        public void DecimalSerialization()
+        {
+            string decimalStr = "10001";
+            var decimalBytes = SerializationHelper.DecimalToBinary(16, decimalStr);
+
+            Int32[] bits = decimal.GetBits(Convert.ToDecimal(decimalStr));
+            List<byte> bytes = new List<byte>();
+            foreach (Int32 i in bits)
+            {
+                bytes.AddRange(BitConverter.GetBytes(i));
+            }
+            var decimalBytes2 = bytes.ToArray();
+
+            var decimalResStr = SerializationHelper.BinaryToDecimal(decimalBytes);
+            Assert.IsTrue(decimalStr == decimalResStr && decimalBytes.SequenceEqual(decimalBytes2));
+        }
+    }
+}
