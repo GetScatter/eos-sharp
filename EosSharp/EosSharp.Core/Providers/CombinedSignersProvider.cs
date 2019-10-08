99d131f9ef7f0b79b81ba7c11acc0ef0dd9669e5
Mário Silva <19377179+mmcs85@users.noreply.github.com>
2019-10-08 18:12:51 +0100

    add doc to CombinedSignersProvider

--- EosSharp/EosSharp.Core/Providers/CombinedSignersProvider.cs
+++ EosSharp/EosSharp.Core/Providers/CombinedSignersProvider.cs
@@ -7,10 +7,17 @@ using System.Linq;
 
 namespace EosSharp.Core.Providers
 {
+    /// <summary>
+    /// Signature provider that combine multiple signature providers to complete all the signatures for a transaction
+    /// </summary>
     public class CombinedSignersProvider : ISignProvider
     {
         private List<ISignProvider> Signers { get; set; }
         
+        /// <summary>
+        /// Creates the provider with a list of signature providers
+        /// </summary>
+        /// <param name="signers"></param>
         public CombinedSignersProvider(List<ISignProvider> signers)
         {
             if (signers == null || signers.Count == 0)
@@ -19,6 +26,10 @@ namespace EosSharp.Core.Providers
             Signers = signers;
         }
 
+        /// <summary>
+        /// Get available public keys from the list of signature providers
+        /// </summary>
+        /// <returns>List of public keys</returns>
         public async Task<IEnumerable<string>> GetAvailableKeys()
         {
             var availableKeysListTasks = Signers.Select(s => s.GetAvailableKeys());
@@ -26,6 +37,14 @@ namespace EosSharp.Core.Providers
             return availableKeysList.SelectMany(k => k).Distinct();
         }
 
+        /// <summary>
+        /// Sign bytes using the list of signature providers
+        /// </summary>
+        /// <param name="chainId">EOSIO Chain id</param>
+        /// <param name="requiredKeys">required public keys for signing this bytes</param>
+        /// <param name="signBytes">signature bytes</param>
+        /// <param name="abiNames">abi contract names to get abi information from</param>
+        /// <returns>List of signatures per required keys</returns>
         public async Task<IEnumerable<string>> Sign(string chainId, IEnumerable<string> requiredKeys, byte[] signBytes, IEnumerable<string> abiNames = null)
         {
             var signatureTasks = Signers.Select(s => s.Sign(chainId, requiredKeys, signBytes, abiNames));
