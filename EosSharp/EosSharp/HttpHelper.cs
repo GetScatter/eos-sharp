b2b0c37cc32a73ef45333e846ae2751d62ee4970
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2020-10-16 22:50:46 +0300

    Update exception

--- EosSharp/EosSharp/HttpHelper.cs
+++ EosSharp/EosSharp/HttpHelper.cs
@@ -226,7 +226,7 @@ namespace EosSharp
             var content = await StreamToStringAsync(stream);
             if(string.IsNullOrEmpty(content)) 
             {
-                throw new NullReferenceException($"Couldn't parse stream data. Content: {content}, Status code: {response.IsSuccessStatusCode}");
+                throw new NullReferenceException($"Couldn't parse stream data. Content: {content}, Status code: {response.StatusCode}");
             }
 
             ApiErrorException apiError = null;
